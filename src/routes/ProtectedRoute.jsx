a35a51e96a8906ed4dda8935ea6c194f86b1a69d
Noman Ahmad <numanafsana19@gmail.com>
2025-11-24 22:14:30 +0600

    protective route created

---
+++ src/routes/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
+
+export default function ProtectedRoute({ children }) {
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <div className="text-center mt-20">Loading...</div>;
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -8,6 +8,7 @@ import MyContribution from "../pages/MyContribution";
 import IssueDetails from "../pages/IssueDetails";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import ProtectedRoute from "./ProtectedRoute";
 
 const router = createBrowserRouter([
   {
@@ -16,10 +17,38 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <Home /> },
       { path: "/issues", element: <AllIssues /> },
-      { path: "/add-issue", element: <AddIssue /> },
-      { path: "/my-issues", element: <MyIssues /> },
-      { path: "/my-contribution", element: <MyContribution /> },
-      { path: "/issue/:id", element: <IssueDetails /> },
+      { 
+        path: "/add-issue", 
+        element: (
+          <ProtectedRoute>
+            <AddIssue />
+          </ProtectedRoute>
+        ),
+      },
+      { 
+        path: "/my-issues", 
+        element: (
+          <ProtectedRoute>
+            <MyIssues />
+          </ProtectedRoute>
+        ),
+      },
+      { 
+        path: "/my-contribution", 
+        element: (
+          <ProtectedRoute>
+            <MyContribution />
+          </ProtectedRoute>
+        ),
+      },
+      { 
+        path: "/issue/:id", 
+        element: (
+          <ProtectedRoute>
+            <IssueDetails />
+          </ProtectedRoute>
+        ),
+      },
       { path: "/login", element: <Login /> },
       { path: "/register", element: <Register /> },
     ],
